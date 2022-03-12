7ab63871c38aeba48754d324e8c2cc9362ed3341
Moein Latifi <moeinl1090@protonmail.com>
2022-03-12 12:44:23 +0330

    completed home xml design

--- app/src/main/java/ir/ariyana/instagram/MainActivity.kt
+++ app/src/main/java/ir/ariyana/instagram/MainActivity.kt
@@ -2,6 +2,7 @@ package ir.ariyana.instagram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import ir.ariyana.instagram.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +12,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.mainActivityBNV.setOnItemSelectedListener {
+            when (it.itemId) {
+
+                R.id.mainBNVHome -> {
+                    Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show()
+                }
+
+                R.id.mainBNVExplore -> {
+                    Toast.makeText(this, "Explore", Toast.LENGTH_SHORT).show()
+                }
+
+                R.id.mainBNVNew -> {
+                    Toast.makeText(this, "New", Toast.LENGTH_SHORT).show()
+                }
+
+                R.id.mainBNVActivity -> {
+                    Toast.makeText(this, "Activity", Toast.LENGTH_SHORT).show()
+                }
+
+                R.id.mainBNVProfile -> {
+                    Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show()
+                }
+            }
+            true
+        }
     }
 }
