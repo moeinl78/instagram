20309135d09aef60af090a768699ab2d6bf8ca09
Moein Latifi <moeinl1090@protonmail.com>
2022-03-13 09:59:09 +0330

    completed story recycler view design

--- app/src/main/java/ir/ariyana/instagram/fragment/FragmentHome.kt
+++ app/src/main/java/ir/ariyana/instagram/fragment/FragmentHome.kt
@@ -30,8 +30,8 @@ class FragmentHome : Fragment() {
 
         val data : ArrayList<HomeItem> = arrayListOf(
             HomeItem("Tesla", "https://media-exp1.licdn.com/dms/image/C4D0BAQHUcu98SZ2TVw/company-logo_200_200/0/1607665771371?e=2147483647&v=beta&t=U60-3BcbnYQD0zrEasbOtVpmXOoxWQly2LMNq3SQOaA", "https://cdn.motor1.com/images/mgl/zOrx0/s3/tesla-roadster.jpg", "340,000", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "924"),
-            HomeItem("Apple", "https://s3-eu-central-1.amazonaws.com/www-staging.esports.com/wp-media-folder-esports-com/app/uploads/2021/11/SonyPC.jpg", "https://support.apple.com/content/dam/edam/applecare/images/en_US/psp/featured-section-give-back-trade-in_2x.jpg", "1,980,000", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "9,841"),
-            HomeItem("Sony", "https://pbs.twimg.com/profile_images/458973611215171584/sbkfC49F_400x400.jpeg", "https://fdn.gsmarena.com/imgroot/reviews/21/sony-xperia-pro-i/preview/lifestyle/-1200w5/gsmarena_018.jpg", "340,000", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "924"),
+            HomeItem("Apple", "https://i.guim.co.uk/img/media/1e7a0b2ce7932ac12684527842a6070a5e9ffb33/161_198_3124_1874/master/3124.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=be815845fe03ec1813e466c83856ba5c", "https://support.apple.com/content/dam/edam/applecare/images/en_US/psp_content/content-block-md-iphone-promo-today-at-apple_2x.png", "1,980,000", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "9,841"),
+            HomeItem("Sony", "https://www.sony.com/image/sonyview1?fmt=png&wid=1200", "https://fdn.gsmarena.com/imgroot/reviews/21/sony-xperia-pro-i/preview/lifestyle/-1200w5/gsmarena_018.jpg", "340,000", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "924"),
             HomeItem("Facebook", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJE8vBPFmLcAKfgR4-l3mPZ0LRrkUfSvhG1w&usqp=CAU", "https://techcrunch.com/wp-content/uploads/2016/06/shutterstock_186292982a.jpg", "340,000", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "924"),
         )
         val adapter = AdapterHome(data)
@@ -42,14 +42,12 @@ class FragmentHome : Fragment() {
             Story("https://www.sony.com/image/sonyview1?fmt=png&wid=1200", "Sony"),
             Story("https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/NASA_logo.svg/1200px-NASA_logo.svg.png", "NASA"),
             Story("https://www.spacex.com/static/images/share.jpg", "SpaceX"),
-            Story("https://www.augsburger-allgemeine.de/img/bilder/crop61777711/0039424940-cv16_9-w940-owebp/Elon-Musk?t=.jpg", "Elon Musk"),
+            Story("https://cdn.britannica.com/45/223045-050-A6453D5D/Telsa-CEO-Elon-Musk-2014.jpg", "Elon Musk"),
             Story("https://upload.wikimedia.org/wikipedia/commons/4/4e/Darren_Hardy.jpg", "Darren Hardy"),
         )
-//        val storyAdapter = AdapterStory(storyData)
-//        binding.fragmentHomeStoryRecyclerView.adapter = storyAdapter
-//        binding.fragmentHomeStoryRecyclerView.layoutManager = LinearLayoutManager(parentFragment?.context, RecyclerView.HORIZONTAL, false)
-        binding.fragmentHomeSendMessage.setOnClickListener {
-            Toast.makeText(parentFragment?.context, "Direct", Toast.LENGTH_SHORT).show()
-        }
+        val storyAdapter = AdapterStory(storyData)
+        binding.fragmentHomeStoryRecyclerView.adapter = storyAdapter
+        binding.fragmentHomeStoryRecyclerView.layoutManager = LinearLayoutManager(parentFragment?.context, RecyclerView.HORIZONTAL, false)
+
     }
 }
